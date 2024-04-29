c0b19496224742915eec6ab528b8ac8e0e485dee
seop02 <88520209+seop02@users.noreply.github.com>
2024-04-30 00:58:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,7 @@ if __name__=='__main__':
             ]
     dev_cut = {
         'KRW-BTC': 1.5e-10, 'KRW-ETH': 3e-9, 'KRW-ETC': 3.5e-6, 'KRW-ANKR': 0.00015, 'KRW-STORJ': 1e-6, 
-        'KRW-MBL': 0.04, 'KRW-MED': 0.001, 'KRW-DKA': 0.024, 'KRW-BTG': 1e-6, 'KRW-GLM': 5.5e-5, 
-        'KRW-NEAR': 4e-6, 'KRW-JST': 0.0007, 'KRW-TFUEL': 0.03, 'KRW-QTUM': 0.003,
-        'KRW-ID': 0.0002, 'KRW-CELO': 0.0005, 'KRW-POWR': 0.0004, 'KRW-PDA': 0.0003, 'KRW-IOST': 0.001,
-        'KRW-ZRX': 1e-6, 'KRW-LSK': 1e-6, 'KRW-HIFI': 1e-6, 'KRW-SOL': 1e-6, 'KRW-CTC': 2e-6,
-        'KRW-MASK': 1e-6, 'KRW-AVAX': 1e-6, 'KRW-APT': 1e-6, 'KRW-STRAX': 1e-6, 'KRW-MVL': 0.0004,
-        'KRW-GRS': 9e-6, 'KRW-PUNDIX': 1e-6, 'KRW-HUNT': 1e-6, 'KRW-SUI': 1e-6, 'KRW-CVC': 1e-6, 'KRW-T': 1e-4,
-        'KRW-TON': 1e-6, 'KRW-WAXP': 4e-5, 'KRW-HBAR': 1e-5, 'KRW-MTL': 1e-4, 'KRW-META': 1e-4, 'KRW-XEM': 1e-4,
-        'KRW-HPO': 2.5e-6, 'KRW-ADA': 3.5e-6
-        }
+    }
     avg_ratio = {
         'KRW-PUNDIX': 0.4e10, 'KRW-ADA':0.1e10
     }
